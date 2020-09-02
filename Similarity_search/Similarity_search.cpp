a277aa0cf7567cea07ec3ccfa0cfad1ecab79c0d
kortez <595976736@qq.com>
2020-09-02 14:12:55 +0800

    update 9.2

--- Similarity_search/Similarity_search.cpp
+++ Similarity_search/Similarity_search.cpp
@@ -1,7 +1,4 @@
-﻿ // Similarity_search.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <fstream> 
 #include <string>
 #include <vector>
@@ -11,17 +8,18 @@
 using namespace std;
 
 
-
 class Token {
+    //单词对象，目前一个token保存一个单词（字符串）
 public:
     string content;
     int hashValue = 0;
-    void initialize(string ct) {
+    Token(string ct) {
         this->content = ct;
     }
 };
 
 void SplitString(const string& s, vector<string>& v, const string& c) {
+    //将字符串s以字符c为分割符切割，结果保存于向量v
     string::size_type head, tail;
     tail = s.find(c);
     head = 0;
@@ -37,6 +35,7 @@ void SplitString(const string& s, vector<string>& v, const string& c) {
  
 
 class Document {
+    //文章对象，一个document保存一系列token
 public:
     vector<Token> tokens;
     vector<string> words;
@@ -50,12 +49,11 @@ public:
             if (words[i] == " ") { //filter
                 continue;
             }
-            Token *t = new Token();
-            t->initialize(words[i]);
+            Token *t = new Token(words[i]);
             tokens.push_back(*t);
         }
     }
-    void initialize(string text) {
+    Document(string text) {
         getWords(text);
         getTokens();
     }
@@ -67,36 +65,47 @@ public:
 };
 
 class Window {
+    //窗口对象，包含起始位置indx1和结束位置indx2，保存决定该窗口哈希值的centerWord的所有位置向量，跨越centerWord数量k，和哈希值
 public:
     unsigned indx1;
     unsigned indx2;
-    unsigned center;
+    vector <unsigned> centerWords;
+    unsigned k;
     int hashValue;
+    bool operator==(Window p)
+    {
+        return this->hashValue == p.hashValue && this->k == p.k;
+    }
+    Window(unsigned indx1, unsigned indx2, vector <unsigned> centerWords, unsigned k, int hashValue) {
+        this->indx1 = indx1;
+        this->indx2 = indx2;
+        this->centerWords = centerWords;
+        this->k = k;
+        this->hashValue = hashValue;
+    }
 };
 
 class WindowPair {
+    //窗口对对象，仅包含两个窗口的下标对
 public:
     unsigned a1;
     unsigned a2;
     unsigned b1;
     unsigned b2;
-    unsigned ca;
-    unsigned cb;
-    WindowPair(unsigned a1,unsigned a2, unsigned b1, unsigned b2, unsigned ca, unsigned cb) {
+    WindowPair(unsigned a1,unsigned a2, unsigned b1, unsigned b2) {
         this->a1 = a1;
         this->a2 = a2;
         this->b1 = b1;
         this->b2 = b2;
-        this->ca = ca;
-        this->cb = cb;
     }
     bool operator==(WindowPair p)
     {
-        return this->a1== p.a1 && this->a2 == p.a2 && this->b1 == p.b1 && this->b2 == p.b2;
+        return this->a1 == p.a1 && this->a2 == p.a2 && this->b1 == p.b1 && this->b2 == p.b2;
     }
 };
 
 class CrashMap {
+    //冲突图，保存窗口对冲撞，以计算出冲撞次数符合要求的区域（对应窗口对）
 public:
     int map[maxLen][maxLen];
 
@@ -109,7 +118,7 @@ public:
             }
         }
     }
-    
+
     void addArea(WindowPair wp) {
         for (size_t i = wp.a1; i <= wp.a2; i++)
         {
@@ -138,15 +147,16 @@ public:
             for (size_t j = 0; j < n; j++)
             {
                 if (map[i][j] >= 400) {
-                    cout << i << " " << j << " " << map[i][j] <<endl;
+                    cout << i << " " << j << " " << map[i][j] << endl;
                 }
-                
+
             }
         }
     }
 };
 
 struct SegmentTree2DNode {
+    //2D片段数，之前用于存储冲撞信息
     int a1;
     int a2;
     int b1;
@@ -158,9 +168,58 @@ struct SegmentTree2DNode {
     SegmentTree2DNode* rd;
 };
 
+void genWindow(unsigned indx1, unsigned indx2, Document d, vector<Window>* w, unsigned ssh) {
+    //新的生成窗口的方法。暴力枚举了所有子窗口
+    if (indx2 < ssh + indx1) {
+        return;
+    }
+    int minVal, value;
+    vector<unsigned> loc;
+    minVal = RAND_MAX;
+    for (vector<Token>::size_type i = indx1; i <= indx2; i++) {
+        value = d.tokens[i].hashValue;
+        if (value < minVal) {
+            loc.clear();
+            loc.push_back(i);
+            minVal = value;
+        }
+        else if (value == minVal) {
+            loc.push_back(i);
+        }
+    }
+
+    for (size_t i = 0; i < loc.size(); i++)
+    {
+        unsigned x, y;
+        for (x = 0; x < loc.at(1); x++)
+        {
+            if (i + 1 >= loc.size()) {
+                break;
+            }
+            for (y = loc.at(i); y < loc.at(i + 1); y++)
+            {
+                if (y < ssh + x) {
+                    continue;
+                }
+                Window* win = new Window(x, y, loc, i + 1, minVal);
+                w->push_back(*win);
+            }
+        }
+    }
+
+    if (loc.at(0) > 0) {   
+        genWindow(indx1, loc.at(0) - 1, d, w, ssh);
+    }
+    if (loc.at(0) < indx2) {
+        genWindow(loc.at(0) + 1, indx2, d, w, ssh);
+    }
+
+
+}
+
 void build2DTree(SegmentTree2DNode* t,int a1,int a2,int b1,int b2,int vals[maxLen][maxLen]) {
+    //旧的创造2D片段树的方法
     if (a1 == a2 && b1 == b2) {
-        //struct SegmentTree2DNode ans = { a1, a2, b1, b2, vals[a1][b1] };
         t->a1 = a1;
         t->a2 = a2;
         t->b1 = b1;
@@ -217,6 +276,7 @@ void build2DTree(SegmentTree2DNode* t,int a1,int a2,int b1,int b2,int vals[maxLe
 }
 
 int query2DTree(SegmentTree2DNode* root ,int a1, int a2, int b1, int b2) {
+    //旧的查询2D片段树的方法
     if (root->a1 == a1 && root->a2 == a2 && root->b1 == b1 && root->b2 == b2) {
         return root->min;
     }
@@ -292,6 +352,7 @@ int query2DTree(SegmentTree2DNode* root ,int a1, int a2, int b1, int b2) {
 }
 
 int RanHash(string text, int seed) {
+    //根据输入生成随机哈希值
     std::hash<std::string> hash_fn;
     int hash = hash_fn(text);
     srand((seed+1)*hash);
@@ -299,14 +360,15 @@ int RanHash(string text, int seed) {
 }
 
 void compareSimilarity(Document d1, Document d2, int seed, vector<WindowPair> &vp, int ssh) {
+    //旧的比较两个document相似度的方法，目前由于存在错误已经不用
     vector<Token>::size_type l1, l2;
     vector<Window> w1, w2;
     
     l1 = d1.tokens.size();
-    l2 = d2.tokens.size();
+    l2 = d2.tokens.size();   
     w1.clear();
     w2.clear();
-    //get hash value     all tokens
+    //get hash value for all tokens
     for (vector<Token>::size_type i = 0; i != l1; i++) {  
         d1.tokens[i].hashValue = RanHash(d1.tokens[i].content, seed);
     }
@@ -350,7 +412,7 @@ void compareSimilarity(Document d1, Document d2, int seed, vector<WindowPair> &v
         if (i2 - i1 < ssh) {
             continue;
         }
-        Window *w = new Window();
+        /*Window *w = new Window();
         if (i1 != lastI1 || i2 != lastI2) {
             w->indx1 = i1;
             w->indx2 = i2;
@@ -359,7 +421,7 @@ void compareSimilarity(Document d1, Document d2, int seed, vector<WindowPair> &v
             w1.push_back(*w);
         }
         lastI1 = i1;
-        lastI2 = i2;
+        lastI2 = i2;*/
         
     }
     lastI1 = lastI2 = -1;
@@ -392,7 +454,7 @@ void compareSimilarity(Document d1, Document d2, int seed, vector<WindowPair> &v
             {
                 break;
             }
-        }
+        }/*
         if (i1 != lastI1 || i2 != lastI2) {
             Window* w = new Window();
             w->indx1 = i1;
@@ -402,7 +464,7 @@ void compareSimilarity(Document d1, Document d2, int seed, vector<WindowPair> &v
             w2.push_back(*w);
         } 
         lastI1 = i1;
-        lastI2 = i2;
+        lastI2 = i2;*/
     }
     /*
     for (vector<Window>::size_type i = 0; i != w1.size(); i++) {
@@ -416,7 +478,7 @@ void compareSimilarity(Document d1, Document d2, int seed, vector<WindowPair> &v
     for (vector<Window>::size_type i = 0; i != w1.size(); i++) {
         for (vector<Window>::size_type j = 0; j != w2.size(); j++) {
             if (w1[i].hashValue == w2[j].hashValue) {
-                WindowPair* wp = new WindowPair(w1[i].indx1, w1[i].indx2, w2[j].indx1, w2[j].indx2,w1[i].center,w2[j].center);
+                WindowPair* wp = new WindowPair(w1[i].indx1, w1[i].indx2, w2[j].indx1, w2[j].indx2);
                 vp.push_back(*wp);
                 ct++;
                 //cout << " [ " << w1[i].indx1 << " , " << w1[i].indx2 << " ] " <<" , " << " [ " << w2[j].indx1 << " , " << w2[j].indx2 << " ] " << endl;
@@ -428,10 +490,9 @@ void compareSimilarity(Document d1, Document d2, int seed, vector<WindowPair> &v
     //cout << "total cash:" << ct << endl;
 }
 
-
-
 int main()
 {   
+    //主程序，命令式交互
     string command;
     vector<Document> dataDocuments;
     vector<WindowPair> resultPairs;
@@ -475,8 +536,7 @@ int main()
                     pos = buf.find("<BODY>");
                     if (buf.find("</BODY>") != buf.npos) {
                         inBody = false;
-                        Document *dd = new Document();
-                        dd->initialize(text);
+                        Document *dd = new Document(text);
                         dataDocuments.push_back(*dd);
                         text = "";
                     }
@@ -495,12 +555,27 @@ int main()
                 ifs.close();
             }
         }
-        else if (command == "rand")
+        else if (command == "neww")
         {
-            SegmentTree2DNode* t = new SegmentTree2DNode();
-            t->a1 = 1;
-            t->a2 = 3;
-            cout << t->a2;
+            vector<Window>* gw1 = new vector<Window>;
+            vector<Window>* gw2 = new vector<Window>;
+            genWindow(0, dataDocuments[0].words.size() - 1, dataDocuments[0], gw1, 3);
+            genWindow(0, dataDocuments[1].words.size() - 1, dataDocuments[1], gw2, 3);
+            CrashMap* cm = new CrashMap();
+            cm->init();
+            for (vector<Window>::size_type i = 0; i < gw1->size(); i++)
+            {
+                for (vector<Window>::size_type j = 0; j < gw2->size(); j++)
+                {
+                    if (gw1->at(i).hashValue == gw2->at(j).hashValue)
+                    {
+                        WindowPair* wp = new WindowPair(gw1->at(i).indx1, gw1->at(i).indx2, gw2->at(j).indx1, gw2->at(j).indx2);
+                        cm->addArea(*wp);  
+                    }
+                    
+                }
+            }
+
         }
         else if (command == "tree")
         {
